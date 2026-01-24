8541a8dc6d9705bd3dd554751b57900711ac4d45
wenjiu <2784307979@qq.com>
2026-01-24 17:32:09 +0800

    chore: bump am-zig and remu

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flake configuration for chip-dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            just
+          ];
+        };
+      }
+    );
+}
